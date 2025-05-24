d4d19c7a25153d90bb9a9808fad28d3a0b89efbf
omgaXD <sladekop@gmail.com>
2025-05-24 23:07:55 +0500

    better map

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,32 +1,36 @@
-// import { load } from '@2gis/mapgl';
-// import { Map } from '@2gis/mapgl/types';
-// import React from 'react';
-// import { useEffect } from 'react';
+import { load } from '@2gis/mapgl';
+import { Map } from '@2gis/mapgl/types';
+import React from 'react';
+import { useEffect } from 'react';
 
-// export const DroneMap = () => {
-//     useEffect(() => {
-//         let map : Map | null = null;
-//         load().then((mapglAPI) => {
-//             map = new mapglAPI.Map('map-container', {
-//                 center: [55.31878, 25.23584],
-//                 zoom: 13,
-//                 key: 'Your API access key',
-//             });
-//         });
+export const DroneMap = () => {
+    useEffect(() => {
+        let map : Map | null = null;
+        load().then((mapglAPI) => {
+            map = new mapglAPI.Map('map-container', {
+                center: [55.31878, 25.23584],
+                zoom: 13,
+                key: '481c2446-3a2e-4a14-be93-31f6d12ced05',
+            });
+        });
 
-//         // Удаляем карту при размонтировании компонента
-//         return () => (map && map.destroy());
-//     }, []);
+        // Удаляем карту при размонтировании компонента
+        return () => {
+            if (map) {
+                map.destroy();
+            }
+        };
+    }, []);
 
-//     return (
-//         <div style={{ width: '100%', height: '100%' }}>
-//             <MapWrapper />
-//         </div>
-//     );
-// };
-// const MapWrapper = React.memo(
-//     () => {
-//         return <div id="map-container" style={{ width: '100%', height: '100%' }}></div>;
-//     },
-//     () => true,
-// );
+    return (
+        <div style={{ width: '100%', height: '100%' }}>
+            <MapWrapper />
+        </div>
+    );
+};
+const MapWrapper = React.memo(
+    () => {
+        return <div id="map-container" style={{ width: '100%', height: '100%' }}></div>;
+    },
+    () => true,
+);
