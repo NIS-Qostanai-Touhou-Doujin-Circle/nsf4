45a3cf36e0b399b4275f03b0bd3ee58b6d3bdfd7
omgaXD <sladekop@gmail.com>
2025-05-25 07:35:29 +0500

    match api

--- app/network/drone.ts
+++ app/network/drone.ts
@@ -2,13 +2,13 @@ import { AddDroneResponse } from '../types/api';
 import { apiUrl } from './consts';
 
 // POST /api/drones - Add a new drone
-export async function addDrone({ url, title, ws }: { url: string; title: string; ws: string }): Promise<AddDroneResponse> {
+export async function addDrone({ url, title, ws }: { url: string; title: string; ws?: string }): Promise<AddDroneResponse> {
     const response = await fetch(`${apiUrl}/drones`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ url, title, ws }),
+        body: JSON.stringify({ rtmp_url: url, title, ws_url: ws }),
     });
 
     if (!response.ok) {
