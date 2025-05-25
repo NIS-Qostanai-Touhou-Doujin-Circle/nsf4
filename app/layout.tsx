79fbf202fa347591f5abd92ea6e3776762e41356
omgaXD <sladekop@gmail.com>
2025-05-25 14:38:16 +0500

    fix double map

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html suppressHydrationWarning lang="en">
+        <html lang="en">
             <head />
             <body
                 className={clsx(
